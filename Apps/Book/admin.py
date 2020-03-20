1aabe1bb6147f576853064a51221124556bb9636
HumoFX <humo_otahanov@icloud.com>
2020-03-20 23:29:19 +0500

    DB create

--- Apps/Book/admin.py
+++ Apps/Book/admin.py
@@ -3,11 +3,14 @@ from .models import Category, UDC, Book
 
 
 # Register your models here.
-class NewAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'udc', 'author', 'special_books')
+    ordering = ('title', 'author')
+    search_fields = ('title', 'udc', 'author', 'date_of_pub', 'date_of_get')
 
 
-admin.site.register(Category, NewAdmin)
+admin.site.register(Category)
 admin.site.register(UDC)
-admin.site.register(Book)
+# admin.site.register(Book)
 
