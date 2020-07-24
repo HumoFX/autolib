5a1cd7ba596d034ce53fd67e6da2a15fdfee4a35
humofx <humo_oatahanov@icloud.com>
2020-07-24 13:38:14 +0500

    Serializers change bug

--- User/serializers.py
+++ User/serializers.py
@@ -5,6 +5,20 @@ from .models import Users, Profile
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = [
+            'id',
+            'last_login',
+            'is_superuser',
+            'username',
+            'full_name',
+            'university_id',
+            'faculty',
+            'passport_serial_id',
+            'tel_num',
+            'email',
+            'is_staff',
+            'is_active',
+            'date_joined',
+        ]
 
 
