dd79d9c5d8c9d7cac1165b1821a098d206845cf3
humofx <humo_oatahanov@icloud.com>
2020-08-17 14:10:26 +0500

    cors ports

--- elib/settings.py
+++ elib/settings.py
@@ -121,7 +121,6 @@ TEMPLATES = [
             'loaders': [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader',
-                'admin_tools.template_loaders.Loader',
             ],
         },
     },
