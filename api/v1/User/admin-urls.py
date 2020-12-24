b9239cd94883c28700b05cf5645b8ce79debfc8b
humofx <humo_oatahanov@icloud.com>
2020-12-25 00:24:38 +0500

    new endpoints

--- api/v1/User/admin-urls.py
+++ api/v1/User/admin-urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 from django.urls import path, include
-from django_registration.views import RegistrationView
+
 
 from .views import UserListAdminView,UserDetailView, RefreshTokenView, ObtainTokenPairView
-from .forms import MyCustomUserForm
 
 urlpatterns = [
     path('user/', UserListAdminView.as_view(), name='list profiles'),
--- api/v1/User/urls.py
+++ api/v1/User/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from django.urls import path, include
 
 from .views import UserCreateView,UserDetailView, RefreshTokenView, ObtainTokenPairView
-from .forms import MyCustomUserForm
 
 urlpatterns = [
     path('user/', UserCreateView.as_view(), name='create profile'),
