e2741da7db57e6a18197265c361ebad69cac68a5
humofx <humo_oatahanov@icloud.com>
2020-12-28 04:52:06 +0500

    cpu usage && university permission

--- api/v1/Order/admin-urls.py
+++ api/v1/Order/admin-urls.py
@@ -3,14 +3,15 @@ from api.v1.Order import views
 
 urlpatterns = [
     path('orders/', views.OrderListAdminView.as_view()),
+    path('orders/short/', views.OrderShortListAdminView.as_view()),
     path('orders/create/', views.OrderCreateAdminView.as_view()),
     path('active_orders/', views.ActiveOrderListAdminView.as_view()),
-    path('active_orders/<int:order_id>', views.ActiveOrderDetailAdminView.as_view()),
+    path('active_orders/<int:order_id>/', views.ActiveOrderDetailAdminView.as_view()),
     path('book_in_use/', views.BookInUseListAdminView.as_view()),
-    path('book_in_use/<int:order_id>', views.BookInUseDetailAdminView.as_view()),
+    path('book_in_use/<int:order_id>/', views.BookInUseDetailAdminView.as_view()),
     path('stats_per_day/', views.StatsPerDay.as_view()),
     path('stats_per_week/', views.StatsPerWeek.as_view()),
     path('stats_per_month/', views.StatsPerMonth.as_view()),
     path('stats_per_year/', views.StatsPerYear.as_view()),
-    path('orders/<int:pk>', views.OrderDetailView.as_view()),
+    path('orders/<int:pk>/', views.OrderDetailView.as_view()),
 ]
